bd13a3084567e1c5e3f0299fdf0bcef4cb5e3160
finn <finn@finnbuntu>
2012-04-23 16:54:52 +0200

    bypass the 64K limit when loading JS -files.

--- src/groovy/jstest/JavascriptTestrunner.groovy
+++ src/groovy/jstest/JavascriptTestrunner.groovy
@@ -14,19 +14,15 @@ class JavascriptTestrunner {
 	
 	JavascriptTestrunner() {
 		Global global = new Global();
-		cx = ContextFactory.getGlobal().enterContext(); //Context.enter()
+		cx = ContextFactory.getGlobal().enterContext();
 		global.init(cx);
 		cx.setLanguageVersion(Context.VERSION_1_6)
-		
-		//if (debug) scope = cx.initStandardObjects(org.mozilla.javascript.tools.shell.Main.getGlobal());
 		scope = cx.initStandardObjects(global)
-		
 		scope.put("Loader", scope, new Loader(cx, scope))
 		cx.setGeneratingSource(true)
 		cx.setGeneratingDebug(true)
 		cx.setGenerateObserverCount(true)
-		//cx.setOptimizationLevel (-1);
-		cx.setOptimizationLevel(9);
+		cx.setOptimizationLevel(-1);
 	}
 	
 	Boolean runTest(String javascriptfile) {
@@ -44,17 +40,13 @@ class JavascriptTestrunner {
 	
 	def runJavascriptFile(Context cx, Scriptable scope, String fileName) {
 		return cx.evaluateReader(scope, new FileReader(fileName), fileName, 1, null);
-		//return cx.evaluateString(scope, new File(fileName).text, fileName, 1, null)
 	}
 	
 	class Loader {
-		//ContextFactory factory = new ContextFactory();
-		
 		Context cx
 		Scriptable scope
 		def load(String fileName) {
 			cx.evaluateReader(scope, new FileReader(fileName), fileName, 1, null);
-			//cx.evaluateString(scope, new File(fileName).text, fileName, 1, null)
 		}
 		def println(String string) {
 			System.out.println(string);
