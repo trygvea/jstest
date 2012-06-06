39dd109f0fd62f2aec49b1354d22b3507a568875
Finn Johnsen <johnsenf@gmail.com>
2012-06-06 21:19:00 +0200

    find the .js -files the right way instead of grabbing them all.

--- src/groovy/jstest/GrailsJavascriptTestType.groovy
+++ src/groovy/jstest/GrailsJavascriptTestType.groovy
@@ -8,32 +8,57 @@ import org.codehaus.groovy.grails.test.event.GrailsTestEventPublisher
 import org.codehaus.groovy.grails.test.support.GrailsTestTypeSupport
 import org.codehaus.groovy.grails.test.report.junit.JUnitReportsFactory
 
-
+// grails test-app unit: simple*
 class GrailsJavascriptTestType extends GrailsTestTypeSupport {
-
 	
 	GrailsJavascriptTestType(String name, String relativeSourcePath) {
-		super(name, relativeSourcePath)
-		//println "${name} ${relativeSourcePath}"
+		super(name, relativeSourcePath + "/js")
+//		println "${name} ${relativeSourcePath}"
 	  }
 	
-	def files
+	def files = []
+	
+//	@Override
+//	protected List<File> findSourceFiles(GrailsTestTargetPattern targetPattern) {
+//	}
+	
+	protected List<String> getTestSuffixes() {
+		return ["Test"]
+	}
+	
+	protected List<String> getTestExtensions() {
+		return ["js"]
+	}
+	
 	@Override
 	protected int doPrepare() {
-		def basedir = new File('test/unit/js')
-		files = basedir.listFiles().grep(~/.*Test.js$/)
+		
+		eachSourceFile { testTargetPattern, sourceFile ->
+			println "${testTargetPattern} ${sourceFile}"
+			files << sourceFile
+		}
+		
+		//def basedir = new File('test/unit/js')
+		//files = basedir.listFiles().grep(~/.*Test.js$/)
 		return files.size()
 	}
 
 	@Override
 	protected GrailsTestTypeResult doRun(GrailsTestEventPublisher eventPublisher) {
-		
 		int failCount = 0
+		
 		for (File currentFile in files) { 
-			println "running ${currentFile.path}"
-			if (!new JavascriptTestrunner().runTest(currentFile.path)) failCount ++
-			
+			//eventPublisher.testCaseStart(currentFile.path)
+			//eventPublisher.testStart(currentFile.path)
+			if (new JavascriptTestrunner().runTest(currentFile.path)) {
+				//eventPublisher.testEnd(currentFile.path)
+			} else {
+				//eventPublisher.testFailure(currentFile.path)
+				failCount ++
+			}
+			//eventPublisher.testCaseEnd(currentFile.path)
 		}
+		
 		return new GrailsJavascriptTestTypeResult(runCount:files.size(), failCount:failCount);
 	}
 }
